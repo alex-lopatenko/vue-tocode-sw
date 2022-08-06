c44fc0b8216551ba687a99c29e141769659aacc3
Alexey <alexey-lopatenko@yandex.ru>
2022-08-06 12:54:31 +0300

    Add services

---
+++ src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="md-body">
+    <h1>Hello home</h1>
+    <p>Edit me</p>
+  </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    axios.get('url').then(() => {})
+  },
+})
+</script>
